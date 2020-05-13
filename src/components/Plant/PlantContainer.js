a842c5d0283fe89a68d71bb75856540f76cffccc
jguzz <joseph.a.guzzardo@gmail.com>
2020-05-13 17:22:12 -0400

    initial components set up
    All running succesfully

---
+++ src/components/Plant/PlantContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function PlantContainer() {
+  return (
+    <div className="PlantContainer">
+      Plant Container
+    </div>
+  );
+}
+
+export default PlantContainer;
---
+++ src/components/User/UserInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function UserInfo() {
+  return (
+    <div className="UserInfo">
+      User Info
+    </div>
+  );
+}
+
+export default UserInfo;
